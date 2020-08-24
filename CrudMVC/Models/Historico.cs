bf6702e01f5a7699f79f44f22f3b8c7675ae2a6f
Eduardo Araujo <consultoreduardosp@gmail.com>
2020-08-24 10:10:27 -0300

    Teste MVC

---
+++ CrudMVC/Models/Historico.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudMVC.Models
+{
+    [Table("Historico")]
+    public class Historico
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("DetalhesHitorico")]
+        [Display(Name = "Detalhes Historico")]
+        public string DetalhesHistorico { get; set; }
+
+        [Column("EmailUsuario")]
+        [Display(Name = "Email Usuario")]
+        public string EmailUsuario { get; set; }
+
+    }
+}
---
+++ CrudMVC/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudMVC.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("ProdutoId")]
+        [Display(Name = "Codigo")]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(150, ErrorMessage = "Maximo (0) caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo (0) caracteres")]
+        public string Nome { get; set; }
+
+        [Column("Valor")]
+        [Display(Name = "Valor")]
+        [DataType(DataType.Currency)]
+        public string Valor { get; set; }
+
+        [DisplayName("Disponivel?")]
+        public bool Disponivel { get; set; }
+    }
+}
